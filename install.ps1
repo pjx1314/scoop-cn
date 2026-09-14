301ed8bf49d337436ab79667eead5b4a3704ba5e
pjx1314 <88777966+pjx1314@users.noreply.github.com>
2026-09-15 04:12:50 +0800

    Update GitHub proxy address in install.ps1

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@
 $scoopDir = $env:SCOOP, "$env:USERPROFILE\scoop" | Where-Object { -not [String]::IsNullOrEmpty($_) } | Select-Object -First 1
 
 # 设置 GitHub 代理地址
-$githubProxy = "https://gh-proxy.com"
+$githubProxy = "https://v4.gh-proxy.org"
 
 # 隐藏进度条
 $global:ProgressPreference = 'SilentlyContinue'
